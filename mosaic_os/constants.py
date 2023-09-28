6518d7a413e9179688fe6ff24fe8aa42d59b0969
Gohar Iqbal <gohar@mosaicventures.com>
2023-09-28 15:37:24 +0100

    Add affinity sandbox constants

--- mosaic_os/constants.py
+++ mosaic_os/constants.py
@@ -7,5 +7,18 @@ affinity_config = {
         "lp_field_priority": 120060,
         "lp_field_owner": 120059,
         "company_field_ec_flag": 3542734,
-    }
+        "company_field_ec_flag_value_on": 9111690,
+        "company_field_ec_flag_value_off": 9111706,
+    },
+    "dev": {
+        "lp_list_id": 181518,
+        "lp_fields": [3443557, 3443561, 3443558],
+        "company_fields": [3651213],
+        "lp_field_status": 3443557,
+        "lp_field_priority": 3443561,
+        "lp_field_owner": 3443558,
+        "company_field_ec_flag": 3651213,
+        "company_field_ec_flag_value_on": 9999246,
+        "company_field_ec_flag_value_off": 9999247,
+    },
 }

bd65d14937e264ba79b913c138eb9a52d46edfbb
Gohar Iqbal <gohar@mosaicventures.com>
2023-10-12 09:27:56 +0100

    Revert "Add field value changes endpoint"
    This reverts commit 72e24f4002b7e7af127bd96d3ae1dace72e0ff15.

--- tests/test_crm.py
+++ tests/test_crm.py
@@ -47,16 +47,6 @@ async def test_affinity_api_get_field_values_raises_error_wrong_param():
     )
 
 
-@pytest.mark.asyncio
-async def test_affinity_api_get_field_value_changes_raises_error_no_field_id():
-    with pytest.raises(ValueError) as excinfo:
-        affinity_client = AffinityApi("test")
-        await affinity_client.get_field_value_changes(params={})
-
-    assert excinfo.type is ValueError
-    assert excinfo.value.args[0] == "Field ID must be specified"
-
-
 # Test if _remove_duplicates removes duplicate dicts in a list
 def test_affinity_remove_duplicates():
     list_with_duplicate_dicts = [{"id": 1}, {"id": 1}, {"id": 2}]

a9c8dac090dba15b10893582bd05f7e1d95c36ae
gohar-mv <132347817+gohar-mv@users.noreply.github.com>
2024-03-07 17:54:12 +0000

    Add more fields to company (#40)

--- mosaic_os/models.py
+++ mosaic_os/models.py
@@ -6,14 +6,20 @@ from mosaic_os.utils import datetime_now
 
 
 class CompanyMetadata(BaseModel):
-    merged_company: dict | None = None
+    merged: bool = False
+    merged_at: datetime | None = None
+    merged_from: list[str] = []
+    merged_by: str
 
 
 class Company(BaseModel):
     id: int | None
+    name: str
+    primary_domain: str
     domains: list[str]
     sp_id: str | None = None
     crm_id: str | None = None
+    current: bool = True
     metadata: CompanyMetadata | None = None
     created_at: datetime = Field(default_factory=datetime_now)
     updated_at: datetime | None = None

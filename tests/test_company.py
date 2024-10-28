fea17edd8586004249da5c490c9efdb9876bec48
gohar-mv <132347817+gohar-mv@users.noreply.github.com>
2024-10-28 14:21:11 +0000

    Add ability to configure G calendar service timeout (#77)
    * Add ability to configure G calendar service timeout
    
    * Fix tests
    
    * Revert to using mocked client

--- tests/test_company.py
+++ tests/test_company.py
@@ -23,7 +23,13 @@ HARMONIC_RETURN_VALUE = {
 }
 
 AFFINITY_SEARCH_RETURN_VALUE = [
-    {"id": 64779194, "name": "Test", "domain": "test.com", "domains": ["test.com"], "global": True}
+    {
+        "id": 64779194,
+        "name": "Test",
+        "domain": "test.com",
+        "domains": ["test.com"],
+        "global": True,
+    }
 ]
 
 AFFINITY_COMPANY_DETAILS_RETURN_VALUE = {
@@ -139,32 +145,57 @@ mock_affinity_config = {
 
 # This tests the case where the company is found in the SP but not in the CRM
 @pytest.mark.asyncio
-async def test_get_all_company_details_no_affinity_match(mocker: MockerFixture, tests_setup_and_teardown):
-    mocker.patch("mosaic_os.sourcing_platform.HarmonicGql.query", return_value=HARMONIC_RETURN_VALUE)
-    mocker.patch("mosaic_os.crm.AffinityApi.search_company_by_name_and_domains", return_value=[])
+async def test_get_all_company_details_no_affinity_match(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
+    mocker.patch(
+        "mosaic_os.sourcing_platform.HarmonicGql.query",
+        return_value=HARMONIC_RETURN_VALUE,
+    )
+    mocker.patch(
+        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains",
+        return_value=[],
+    )
 
-    company_details = await get_all_company_details("test.com", mock_affinity_config)
+    company_details = await get_all_company_details(
+        "test.com", mock_affinity_config
+    )
     assert company_details["crm"] is None
     assert company_details["sourcing_platform"] is not None
     assert company_details["sourcing_platform"]["name"] == "Test"
-    assert company_details["sourcing_platform"]["website"]["domain"] == "test.com"
-    assert company_details["sourcing_platform"]["watchlists"][0]["name"] == "test"
+    assert (
+        company_details["sourcing_platform"]["website"]["domain"] == "test.com"
+    )
+    assert (
+        company_details["sourcing_platform"]["watchlists"][0]["name"] == "test"
+    )
 
 
 # This tests the case where the company is found in the CRM and in the SP
 @pytest.mark.asyncio
-async def test_get_all_company_details_sp_and_crm_match(mocker: MockerFixture, tests_setup_and_teardown):
-    mocker.patch("mosaic_os.sourcing_platform.HarmonicGql.query", return_value=HARMONIC_RETURN_VALUE)
+async def test_get_all_company_details_sp_and_crm_match(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     mocker.patch(
-        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains", return_value=AFFINITY_SEARCH_RETURN_VALUE
+        "mosaic_os.sourcing_platform.HarmonicGql.query",
+        return_value=HARMONIC_RETURN_VALUE,
     )
-    mocker.patch("mosaic_os.crm.AffinityApi.get_company_details", return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE)
+    mocker.patch(
+        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains",
+        return_value=AFFINITY_SEARCH_RETURN_VALUE,
+    )
+    mocker.patch(
+        "mosaic_os.crm.AffinityApi.get_company_details",
+        return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE,
+    )
     mocker.patch(
         "mosaic_os.crm.AffinityApi.get_field_values",
         return_value=AFFINITY_COMPANY_FIELD_VALUES_RETURN_VALUE,
     )
 
-    company_details = await get_all_company_details("test.com", mock_affinity_config)
+    company_details = await get_all_company_details(
+        "test.com", mock_affinity_config
+    )
 
     assert company_details["crm"] is not None
     assert company_details["sourcing_platform"] is not None
@@ -172,15 +203,27 @@ async def test_get_all_company_details_sp_and_crm_match(mocker: MockerFixture, t
     assert company_details["crm"]["company_name"] == "Test"
     assert company_details["crm"]["domain"] == "test.com"
     assert isinstance(company_details["crm"]["ec_flag"], dict)
-    assert company_details["crm"]["last_live_pipeline_list_entry"]["entry_id"] == 390
-    assert isinstance(company_details["crm"]["last_live_pipeline_list_entry"]["status"], dict)
-    assert isinstance(company_details["crm"]["last_live_pipeline_list_entry"]["owner"], dict)
-    assert isinstance(company_details["crm"]["last_live_pipeline_list_entry"]["priority"], dict)
+    assert (
+        company_details["crm"]["last_live_pipeline_list_entry"]["entry_id"]
+        == 390
+    )
+    assert isinstance(
+        company_details["crm"]["last_live_pipeline_list_entry"]["status"], dict
+    )
+    assert isinstance(
+        company_details["crm"]["last_live_pipeline_list_entry"]["owner"], dict
+    )
+    assert isinstance(
+        company_details["crm"]["last_live_pipeline_list_entry"]["priority"],
+        dict,
+    )
 
 
 # This tests the case where the company is found in the CRM but not in the SP
 @pytest.mark.asyncio
-async def test_get_all_company_details_no_sp_match(mocker: MockerFixture, tests_setup_and_teardown):
+async def test_get_all_company_details_no_sp_match(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     harmonic_company_not_found_error = [
         {
             "message": "404: Not Found",
@@ -204,18 +247,26 @@ async def test_get_all_company_details_no_sp_match(mocker: MockerFixture, tests_
     ]
     mocker.patch(
         "mosaic_os.sourcing_platform.HarmonicGql.query",
-        side_effect=TransportQueryError(msg="404: Not Found", errors=harmonic_company_not_found_error),
+        side_effect=TransportQueryError(
+            msg="404: Not Found", errors=harmonic_company_not_found_error
+        ),
     )
     mocker.patch(
-        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains", return_value=AFFINITY_SEARCH_RETURN_VALUE
+        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains",
+        return_value=AFFINITY_SEARCH_RETURN_VALUE,
     )
-    mocker.patch("mosaic_os.crm.AffinityApi.get_company_details", return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE)
+    mocker.patch(
+        "mosaic_os.crm.AffinityApi.get_company_details",
+        return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE,
+    )
     mocker.patch(
         "mosaic_os.crm.AffinityApi.get_field_values",
         return_value=AFFINITY_COMPANY_FIELD_VALUES_RETURN_VALUE,
     )
 
-    company_details = await get_all_company_details("test.com", mock_affinity_config)
+    company_details = await get_all_company_details(
+        "test.com", mock_affinity_config
+    )
 
     assert company_details["crm"] is not None
     assert (
@@ -225,7 +276,9 @@ async def test_get_all_company_details_no_sp_match(mocker: MockerFixture, tests_
 
 
 @pytest.mark.asyncio
-async def test_get_all_company_details_no_sp_and_no_enrichment_urn(mocker: MockerFixture, tests_setup_and_teardown):
+async def test_get_all_company_details_no_sp_and_no_enrichment_urn(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     harmonic_company_not_found_error = [
         {
             "message": "404: Not Found",
@@ -244,25 +297,35 @@ async def test_get_all_company_details_no_sp_and_no_enrichment_urn(mocker: Mocke
     ]
     mocker.patch(
         "mosaic_os.sourcing_platform.HarmonicGql.query",
-        side_effect=TransportQueryError(msg="404: Not Found", errors=harmonic_company_not_found_error),
+        side_effect=TransportQueryError(
+            msg="404: Not Found", errors=harmonic_company_not_found_error
+        ),
     )
     mocker.patch(
-        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains", return_value=AFFINITY_SEARCH_RETURN_VALUE
+        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains",
+        return_value=AFFINITY_SEARCH_RETURN_VALUE,
     )
-    mocker.patch("mosaic_os.crm.AffinityApi.get_company_details", return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE)
+    mocker.patch(
+        "mosaic_os.crm.AffinityApi.get_company_details",
+        return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE,
+    )
     mocker.patch(
         "mosaic_os.crm.AffinityApi.get_field_values",
         return_value=AFFINITY_COMPANY_FIELD_VALUES_RETURN_VALUE,
     )
 
-    company_details = await get_all_company_details("test.com", mock_affinity_config)
+    company_details = await get_all_company_details(
+        "test.com", mock_affinity_config
+    )
 
     assert company_details["crm"] is not None
     assert company_details["sourcing_platform"]["enrichment_urn"] is None
 
 
 @pytest.mark.asyncio
-async def test_get_all_company_details_no_sp_status_422(mocker: MockerFixture, tests_setup_and_teardown):
+async def test_get_all_company_details_no_sp_status_422(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     harmonic_company_not_found_error = [
         {
             "message": "422: Unprocessable Entity",
@@ -281,28 +344,42 @@ async def test_get_all_company_details_no_sp_status_422(mocker: MockerFixture, t
     ]
     mocker.patch(
         "mosaic_os.sourcing_platform.HarmonicGql.query",
-        side_effect=TransportQueryError(msg="404: Not Found", errors=harmonic_company_not_found_error),
+        side_effect=TransportQueryError(
+            msg="404: Not Found", errors=harmonic_company_not_found_error
+        ),
     )
     mocker.patch(
-        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains", return_value=AFFINITY_SEARCH_RETURN_VALUE
+        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains",
+        return_value=AFFINITY_SEARCH_RETURN_VALUE,
     )
-    mocker.patch("mosaic_os.crm.AffinityApi.get_company_details", return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE)
+    mocker.patch(
+        "mosaic_os.crm.AffinityApi.get_company_details",
+        return_value=AFFINITY_COMPANY_DETAILS_RETURN_VALUE,
+    )
     mocker.patch(
         "mosaic_os.crm.AffinityApi.get_field_values",
         return_value=AFFINITY_COMPANY_FIELD_VALUES_RETURN_VALUE,
     )
 
-    company_details = await get_all_company_details("test.com", mock_affinity_config)
+    company_details = await get_all_company_details(
+        "test.com", mock_affinity_config
+    )
 
     assert company_details["crm"] is not None
     assert company_details["sourcing_platform"]["enrichment_urn"] is None
 
 
 @pytest.mark.asyncio
-async def test_company_found_in_crm_but_no_live_pipeline_entry_found(mocker: MockerFixture, tests_setup_and_teardown):
-    mocker.patch("mosaic_os.sourcing_platform.HarmonicGql.query", return_value=HARMONIC_RETURN_VALUE)
+async def test_company_found_in_crm_but_no_live_pipeline_entry_found(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     mocker.patch(
-        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains", return_value=AFFINITY_SEARCH_RETURN_VALUE
+        "mosaic_os.sourcing_platform.HarmonicGql.query",
+        return_value=HARMONIC_RETURN_VALUE,
+    )
+    mocker.patch(
+        "mosaic_os.crm.AffinityApi.search_company_by_name_and_domains",
+        return_value=AFFINITY_SEARCH_RETURN_VALUE,
     )
     mocker.patch(
         "mosaic_os.crm.AffinityApi.get_company_details",
@@ -313,7 +390,9 @@ async def test_company_found_in_crm_but_no_live_pipeline_entry_found(mocker: Moc
         return_value=AFFINITY_COMPANY_FIELD_VALUES_RETURN_VALUE,
     )
 
-    company_details = await get_all_company_details("test.com", mock_affinity_config)
+    company_details = await get_all_company_details(
+        "test.com", mock_affinity_config
+    )
 
     assert company_details["crm"] is not None
     assert company_details["crm"]["company_id"] == 64779194
@@ -323,20 +402,29 @@ async def test_company_found_in_crm_but_no_live_pipeline_entry_found(mocker: Moc
     assert company_details["crm"]["last_live_pipeline_list_entry"] is None
 
 
-def test_lookup_company_master_id_by_domain_no_match(mocker: MockerFixture, tests_setup_and_teardown):
+def test_lookup_company_master_id_by_domain_no_match(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     client = mocker.patch("tests.test_company.Client")
 
     company = lookup_company_master_id_by_domain("test.com", client)
     assert company is None
 
 
-def test_lookup_company_master_id_by_domain_match(mocker: MockerFixture, tests_setup_and_teardown):
+def test_lookup_company_master_id_by_domain_match(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     client = mocker.patch("tests.test_company.Client")
     client.key.return_value = Key("Company", 123, project="test-project")
     company_entity = Entity(key=client.key("Company", 123))
     company_entity.update(
         Company(
-            id=123, name="Test", primary_domain="test.com", domains=["test.com"], sp_id="12345", crm_id="12"
+            id=123,
+            name="Test",
+            primary_domain="test.com",
+            domains=["test.com"],
+            sp_id="12345",
+            crm_id="12",
         ).model_dump(exclude={"id"})
     )
     existing_company_record = [company_entity]
@@ -349,14 +437,23 @@ def test_lookup_company_master_id_by_domain_match(mocker: MockerFixture, tests_s
     assert company.crm_id == "12"
 
 
-def test_create_company_master_id(mocker: MockerFixture, tests_setup_and_teardown):
+def test_create_company_master_id(
+    mocker: MockerFixture, tests_setup_and_teardown
+):
     client = mocker.patch("tests.test_company.Client")
     mocked_key = Key("Company", 123, project="test-project")
     client.allocate_ids.return_value = [mocked_key]
     client.entity.return_value = Entity(key=mocked_key)
     client.put.return_value = None
 
-    company = Company(id=None, name="Test", primary_domain="test.com", domains=["test.com"], sp_id="12345", crm_id="12")
+    company = Company(
+        id=None,
+        name="Test",
+        primary_domain="test.com",
+        domains=["test.com"],
+        sp_id="12345",
+        crm_id="12",
+    )
     company = create_company_master_id(company, client)
 
     assert company.id == 123
